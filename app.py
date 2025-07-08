5cfd77e808fa6bc74b133ae28af8e37ed994ca46
Kaitlyn Hou <144543259+k8tlynh0u@users.noreply.github.com>
2025-07-08 15:59:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -118,9 +118,9 @@ st.title("📰 kaitlyn's daily news report")
 st.markdown("""
 track news mentions for any public figure + get an AI summary/sentiment report
 
-enter a name, date, and email to get started.
+enter a name, date, and email to get started
 
-*refrain from entering today's date for optimal functionality.*
+*refrain from entering today's date for optimal functionality*
 """)
 
 col1, col2 = st.columns(2)
